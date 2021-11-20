04d06f1b74e10ec7cfb55766721550849efbafdc
mehrdad <mehrdadmd27@gmail.com>
2021-11-20 23:59:47 +0330

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Select from "react-select/dist/declarations/src/Select";
+
 const App = () => {
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
   return <div className="container">
     <button className="btn btn-danger">heeelo</button>
+    <Select options={options} />
   </div>;
 };
 
